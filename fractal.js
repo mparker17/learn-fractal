975a602d5a70486cc7cc69eeb799a92be699dd38
M Parker <mparker17@users.noreply.github.com>
2019-10-18 11:13:04 -0400

    Add a component library build directory.

--- fractal.js
+++ fractal.js
@@ -11,3 +11,5 @@ fractal.components.engine(twigAdapter);
 fractal.components.set('ext','.twig');
 
 fractal.docs.set('path', __dirname + '/src/docs');
+
+fractal.web.set('builder.dest', __dirname + '/build');
