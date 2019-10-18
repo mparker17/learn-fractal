bf55888d68c39fb80d9f0b27bcbbab00186b4e12
M Parker <mparker17@users.noreply.github.com>
2019-10-18 09:27:57 -0400

    Switch Fractal to use twig.

--- fractal.js
+++ fractal.js
@@ -1,6 +1,13 @@
 'use strict';
 
 const fractal = module.exports = require('@frctl/fractal').create();
+const consolidate = require('@frctl/consolidate');
+const twigAdapter = consolidate('twig');
+
 fractal.set('project.title', 'Learn fractal');
+
 fractal.components.set('path', __dirname + '/src/components');
+fractal.components.engine(twigAdapter);
+fractal.components.set('ext','.twig');
+
 fractal.docs.set('path', __dirname + '/src/docs');
